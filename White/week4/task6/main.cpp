7c99da2b2a39046c909f7f16f72f054c87b6fd69
Nazar0vIvan <vano888888@gmail.com>
2022-11-07 22:53:50 +0300

    add white-w4t6

---
+++ White/week4/task6/main.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  ifstream input("input.txt");
+
+  double a;
+  cout << fixed << setprecision(3);
+  while (input >> a) {
+    cout << a << endl;
+  }
+
+  return 0;
+}
