17da61975d280c3f719f6a467c8ae20ef63a0428
BormachBoroda <vano888888@gmail.com>
2022-11-08 13:37:58 +0300

    add yellow-w1t7

---
+++ Yellow/week1/task7/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <string>
+#include <map>
+#include <exception>
+
+using namespace std;
+
+template<typename Key, typename Value>
+Value& GetRefStrict(map<Key,Value>& m, const Key& key);
+
+template<typename Key, typename Value>
+Value& GetRefStrict(map<Key,Value>& m, const Key& key){
+    if(!m.count(key)){
+        throw runtime_error("");
+    }
+    return m[key];
+}
+
+int main()
+{
+    map<int, string> m = {{0, "value"}};
+    string& item = GetRefStrict(m, 0);
+    item = "newvalue";
+    cout << m[0] << endl; // выведет newvalue
+
+    return 0;
+}
+
