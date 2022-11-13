4ca3c84525493b5db771e7670ec63b8b50a00324
Nazar0vIvan <vano888888@gmail.com>
2022-11-14 00:35:04 +0300

    add red-w1t7

---
+++ Red/week1/task7/main.cpp
@@ -0,0 +1,57 @@
+#include <vector>
+#include <utility>
+
+#include "test_runner.h"
+
+using namespace std;
+
+template <typename T>
+class Table {
+public:
+  Table(size_t rows, size_t columns) {
+    Resize(rows, columns);
+  }
+
+  const vector<T>& operator [] (size_t idx) const {
+    return data[idx];
+  }
+
+  vector<T>& operator [] (size_t idx) {
+    return data[idx];
+  }
+
+  void Resize(size_t rows, size_t columns) {
+    data.resize(rows);
+    for (auto& item : data) {
+      item.resize(columns);
+    }
+  }
+
+  pair<size_t, size_t> Size() const {
+    if (!data.empty() && !data[0].empty()) {
+      return {data.size(), data[0].size()};
+    }
+    return  {0, 0};
+  }
+
+private:
+  vector<vector<T>> data;
+
+};
+
+void TestTable() {
+  Table<int> t(1, 1);
+  ASSERT_EQUAL(t.Size().first, 1u);
+  ASSERT_EQUAL(t.Size().second, 1u);
+  t[0][0] = 42;
+  ASSERT_EQUAL(t[0][0], 42);
+  t.Resize(3, 4);
+  ASSERT_EQUAL(t.Size().first, 3u);
+  ASSERT_EQUAL(t.Size().second, 4u);
+}
+
+int main() {
+  TestRunner tr;
+  RUN_TEST(tr, TestTable);
+  return 0;
+}
