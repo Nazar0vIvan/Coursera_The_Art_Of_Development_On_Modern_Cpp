a33e3a2608b57f24d5264dc41050b9d347b00186
BormachBoroda <vano888888@gmail.com>
2022-12-26 16:17:46 +0300

    add red-w2t4

--- Red/week2/task4/main.cpp
+++ Red/week2/task4/main.cpp
@@ -8,101 +8,60 @@
 
 using namespace std;
 
-class ReadingManager {
-public:
-    ReadingManager() : user_page_counts_(MAX_USER_COUNT_ + 1, 0), sorted_users_(), user_positions_(MAX_USER_COUNT_ + 1, -1) {}
-
-    void Read(int user_id, int page_count) {
-        if (user_page_counts_[user_id] == 0) {
-          AddUser(user_id); // amortized O(1)
-        }
-        user_page_counts_[user_id] = page_count; // O(1)
-        int& position = user_positions_[user_id]; // O(1)
-        while (position > 0 && page_count > user_page_counts_[sorted_users_[position - 1]]) {
-            SwapUsers(position, position - 1); // O(1)
-        }
-    }
-    // O()
-
-    double Cheer(int user_id) const {
-        if (user_page_counts_[user_id] == 0) {
-            return 0;
-        }
-        const int user_count = GetUserCount();
-        if (user_count == 1) {
-            return 1;
-        }
-        const int page_count = user_page_counts_[user_id];
-        int position = user_positions_[user_id];
-        while (position < user_count && user_page_counts_[sorted_users_[position]] == page_count) {
-            ++position;
-        }
-        if (position == user_count) {
-            return 0;
-        }
-        return (user_count - position) * 1.0 / (user_count - 1);
-    }
-
-private:
-    static const int MAX_USER_COUNT_ = 100'000;
-
-    vector<int> user_page_counts_;
-    vector<int> sorted_users_;   // отсортированы по убыванию количества страниц
-    vector<int> user_positions_; // позиции в векторе sorted_users_
-
-    int GetUserCount() const
-    {
-        return sorted_users_.size();
-    }
-    void AddUser(int user_id)
-    {
-        sorted_users_.push_back(user_id);
-        user_positions_[user_id] = sorted_users_.size() - 1;
-    }
-    void SwapUsers(int lhs_position, int rhs_position) // O(1)
-    {
-        const int lhs_id = sorted_users_[lhs_position];
-        const int rhs_id = sorted_users_[rhs_position];
-        swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
-        swap(user_positions_[lhs_id], user_positions_[rhs_id]);
-    }
-};
-
-/*
 class ReadingManager{
 public:
-    ReadingManager() : usersIds(MAX_USERS_COUNT+1, -1), pagesCount(MAX_PAGES_COUNT+1, 0){}
-    void Read(int user_id, int page_count){
-        for (int i = usersIds[user_id] + 1; i <= page_count; i++)
-        {
-            ++pagesCount[i];
-        }
-        usersIds[user_id] = page_count;
+  ReadingManager() : user_page_counts_(MAX_USER_COUNT_ + 1, -1), page_achieved_by_count_(MAX_PAGE_COUNT_ + 1, 0) {}
+
+  void Read(int user_id, int page_count) {
+    UpdatePageRange(user_page_counts_[user_id] + 1, page_count + 1);
+    user_page_counts_[user_id] = page_count;
+  }
+
+  double Cheer(int user_id) const {
+    const int pages_count = user_page_counts_[user_id];
+    if (pages_count == -1) {
+      return 0;
     }
-    double Cheer(int user_id){
-        if (usersIds[user_id] == -1){ // если user_id нет
-            return 0.0f;
-        }
-        if(pagesCount[0] == 1){ // если только один пользователь прочитал 1-ую страницу
-            return 1.0f;
-        }
-        // если есть ещё пользователи кроме user_id
-        return 1.0f*(pagesCount[0]-pagesCount[usersIds[user_id]])/(pagesCount[0] - 1);
+    const int user_count = GetUserCount();
+    if (user_count == 1) {
+      return 1;
     }
+    // По умолчанию деление целочисленное, поэтому
+    // нужно привести числитель к типу double.
+    // Простой способ сделать это — умножить его на 1.0.
+    return (user_count - page_achieved_by_count_[pages_count]) * 1.0 / (user_count - 1);
+  }
 
 private:
-    int MAX_USERS_COUNT = 100000;
-    int MAX_PAGES_COUNT = 1000;
+  // Статическое поле не принадлежит какому-то конкретному
+  // объекту класса. По сути это глобальная переменная,
+  // в данном случае константная.
+  // Будь она публичной, к ней можно было бы обратиться снаружи
+  // следующим образом: ReadingManager::MAX_USER_COUNT.
+  static const int MAX_USER_COUNT_ = 100'000;
+  static const int MAX_PAGE_COUNT_ = 1'000;
 
-    vector<int> usersIds; // количетсво страниц, которые суммарно прочитал i-ый пользователь
-    // usersIds[0] - количество страниц, который прочитал 1-ый пользователь
-    vector<int> pagesCount; // количество пользователей, которые прочитали i-ую страницу
-    // pagesCount[0] - количество пользователей, которые прочитали 1-ую страницу
+  // Номер страницы, до которой дочитал пользователь <ключ>
+  vector<int> user_page_counts_;
+  // Количество пользователей, дочитавших (как минимум) до страницы <индекс>
+  vector<int> page_achieved_by_count_;
 
+  int GetUserCount() const {
+    return page_achieved_by_count_[0];
+  }
+
+  // lhs включительно, rhs не включительно
+  void UpdatePageRange(int lhs, int rhs) {
+    for (int i = lhs; i < rhs; ++i) {
+      ++page_achieved_by_count_[i];
+    }
+  }
 };
-*/
 
 int main() {
+  // Для ускорения чтения данных отключается синхронизация
+  // cin и cout с stdio,
+  // а также выполняется отвязка cin от cout
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
@@ -120,7 +79,8 @@ int main() {
       int page_count;
       cin >> page_count;
       manager.Read(user_id, page_count);
-    } else if (query_type == "CHEER") {
+    }
+    else if (query_type == "CHEER") {
       cout << setprecision(6) << manager.Cheer(user_id) << "\n";
     }
   }
